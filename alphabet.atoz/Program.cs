9246409aada979119b505c859456cd03a8647586
discolarrence <discolarrence@gmail.com>
2022-01-25 12:45:43 -0500

    finished ztoa

--- alphabet.atoz/Program.cs
+++ alphabet.atoz/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Text;
 
-namespace alphabet.atoz
+namespace alphabet
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            AToZ();
+            ZToA();
+            Console.ReadLine();
+        }
+
+        private static void AToZ()
         {
             string alphabet;
             var builder = new StringBuilder();
@@ -18,5 +25,20 @@ namespace alphabet.atoz
             alphabet = builder.ToString();
             Console.WriteLine($"{alphabet}");
         }
+
+        private static void ZToA()
+        {
+            string backwardsAlphabet;
+            var builder = new StringBuilder();
+            char letter = 'z';
+            while (letter >= 'a')
+            {
+                builder.Append(letter);
+                letter--;
+            }
+            backwardsAlphabet = builder.ToString();
+            Console.WriteLine($"{backwardsAlphabet}");
+        }
+   
     }
 }
