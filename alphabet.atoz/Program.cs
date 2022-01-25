537a152df2f9207315617ed1210e9a3ebafee119
discolarrence <discolarrence@gmail.com>
2022-01-25 12:36:19 -0500

    finished atoz

---
+++ alphabet.atoz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace alphabet.atoz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alphabet;
+            var builder = new StringBuilder();
+            char letter = 'a';
+            while (letter <= 'z')
+            {
+                builder.Append(letter);
+                letter++;
+            }
+            alphabet = builder.ToString();
+            Console.WriteLine($"{alphabet}");
+        }
+    }
+}
