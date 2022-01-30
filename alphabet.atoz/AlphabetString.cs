08f97c2bcada9f5b015da72fe350ba27e5b8890a
discolarrence <discolarrence@gmail.com>
2022-01-29 23:25:35 -0500

    changed classes

---
+++ alphabet.atoz/AlphabetString.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace alphabet
+{
+    class AlphabetString
+    {
+        public static string AToZ()
+        {
+            StringBuilder builder = new(); 
+            char letter = 'A';
+            while (letter <= 'Z')
+            {
+                builder.Append(letter);
+                letter++;
+            }
+            return builder.ToString();
+        }
+
+        public static string ZToA()
+        {
+            StringBuilder builder = new(); 
+            char letter = 'Z';
+            while (letter >= 'A')
+            {
+                builder.Append(letter);
+                letter--;
+            }
+            return builder.ToString();
+        }
+
+        public static string AToZEveyOther()
+        {
+            StringBuilder builder = new(); 
+            char letter = 'A';
+            while (letter <= 'Z')
+            {
+                builder.Append(letter);
+                letter = (char)(letter + 2);
+            }
+            return builder.ToString();
+        }
+    }
+}
--- alphabet.atoz/Program.cs
+++ alphabet.atoz/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace alphabet
 {
@@ -7,52 +6,9 @@ namespace alphabet
     {
         static void Main(string[] args)
         {
-            AToZ();
-            ZToA();
-            AToZEveyOther();
-        }
-
-        private static void AToZ()
-        {
-            string alphabet;
-            var builder = new StringBuilder();
-            char letter = 'A';
-            while (letter <= 'Z')
-            {
-                builder.Append(letter);
-                letter++;
-            }
-            alphabet = builder.ToString();
-            Console.WriteLine($"{alphabet}");
-        }
-
-        private static void ZToA()
-        {
-            string backwardsAlphabet;
-            var builder = new StringBuilder();
-            char letter = 'Z';
-            while (letter >= 'A')
-            {
-                builder.Append(letter);
-                letter--;
-            }
-            backwardsAlphabet = builder.ToString();
-            Console.WriteLine($"{backwardsAlphabet}");
-        }
-
-        private static void AToZEveyOther()
-        {
-            string alphabet;
-            var builder = new StringBuilder();
-            char letter = 'A';
-            while (letter <= 'Z')
-            {
-                builder.Append(letter);
-                letter++;
-                letter++;
-            }
-            alphabet = builder.ToString();
-            Console.WriteLine($"{alphabet}");
+            Console.WriteLine(AlphabetString.AToZ()); ;
+            Console.WriteLine(AlphabetString.ZToA()); ;
+            Console.WriteLine(AlphabetString.AToZEveyOther()); 
         }
 
     }
