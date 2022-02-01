abdc9a108679a9c86ba0df954d970ffa013d65c7
discolarrence <discolarrence@gmail.com>
2022-01-31 19:53:30 -0500

    added ValVRefTypes Project

---
+++ ValueVsRefTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValueVsRefTypes
+{
+    public class Person
+    {
+        public byte Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Age = 21;
+            Console.WriteLine($"p1.Age = {p1.Age}");
+            Person p1Copy = p1;
+            p1Copy.Age = 55;
+            Console.WriteLine($"p1Copy.Age = {p1Copy.Age}");
+            // Does this change if Person is a class instead of a struct
+            Console.WriteLine($"p1.Age = {p1.Age}");
+        }
+    }
+}
