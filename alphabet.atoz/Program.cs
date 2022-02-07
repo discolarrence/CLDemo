59ec39f0fdc847533b04d675b086e80ac6f69a90
discolarrence <discolarrence@gmail.com>
2022-02-06 21:31:19 -0500

    added user interaction

--- alphabet.atoz/Program.cs
+++ alphabet.atoz/Program.cs
@@ -7,15 +7,71 @@ namespace alphabet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(AToZ()); ;
-            Console.WriteLine(ZToA()); ;
-            Console.WriteLine(AToZEveyOther()); 
+            string mainMenuChoice = "";
+
+            while (mainMenuChoice != "0")
+            {
+                Console.WriteLine("Type a number for what you want to see.\n" +
+                    "1 Alphabetical Order\n" +
+                    "2 Backwards Alphabetical Order\n" +
+                    "3 Alphabetical Order with Skipped Letters\n" +
+                    "0 Exit");
+
+                mainMenuChoice = Console.ReadLine();
+
+                if (mainMenuChoice == "1")
+                {
+                    char startLetter = GetStartLetter();
+                    Console.WriteLine($"\n{ AToZ(startLetter) }\n");
+                }
+                if (mainMenuChoice == "2")
+                {
+                    char startLetter = GetStartLetter();
+                    Console.WriteLine($"\n{ ZToA(startLetter) }\n");
+                }
+                if (mainMenuChoice == "3")
+                {
+                    char startLetter = GetStartLetter();
+                    int skipInt = GetSkipNumber();
+                    Console.WriteLine($"\n{ AToZSkip(skipInt, startLetter) }\n");
+                }
+
+            }
+
         }
 
-        public static string AToZ()
+        private static int GetSkipNumber()
+        {
+            int skipInt = 0;
+            while (skipInt == 0)
+            {
+                Console.WriteLine("How many letters would you like to skip?");
+                string skipString = Console.ReadLine();
+                int.TryParse(skipString, out skipInt);
+            }
+
+            return skipInt;
+        }
+
+        private static char GetStartLetter()
+        {
+            bool validChar = false;
+            char startLetterChar = '0';
+
+            while (char.ToUpper(startLetterChar) < 'A' || char.ToUpper(startLetterChar) > 'Z' || validChar == false)
+            {
+                Console.WriteLine("What is the starting letter?");
+                string startLetterString = Console.ReadLine();
+                validChar = char.TryParse(startLetterString, out startLetterChar);
+
+            }
+            return char.ToUpper(startLetterChar);
+        }
+
+        public static string AToZ(char startLetter)
         {
             StringBuilder builder = new();
-            char letter = 'A';
+            char letter = startLetter;
             while (letter <= 'Z')
             {
                 builder.Append(letter);
@@ -24,10 +80,10 @@ namespace alphabet
             return builder.ToString();
         }
 
-        public static string ZToA()
+        public static string ZToA(char startLetter)
         {
             StringBuilder builder = new();
-            char letter = 'Z';
+            char letter = startLetter;
             while (letter >= 'A')
             {
                 builder.Append(letter);
@@ -36,14 +92,14 @@ namespace alphabet
             return builder.ToString();
         }
 
-        public static string AToZEveyOther()
+        public static string AToZSkip(int step, char startLetter)
         {
             StringBuilder builder = new();
-            char letter = 'A';
+            char letter = startLetter;
             while (letter <= 'Z')
             {
                 builder.Append(letter);
-                letter = (char)(letter + 2);
+                letter = (char)(letter + step);
             }
             return builder.ToString();
         }
